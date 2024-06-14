41f74c32e5f91b0bebc2c624583858e5f4ea1f47
Jacute <you@example.com>
2024-06-14 14:55:11 +0700

    update reverse | flagger

--- reverse/flagger/task/src/Flagger.java
+++ reverse/flagger/task/src/Flagger.java
@@ -1,13 +1,6 @@
 import java.util.Scanner;
 
 public class Flagger {
-    public static String bytesToHex(byte[] in) {
-        final StringBuilder builder = new StringBuilder();
-        for(byte b : in) {
-            builder.append(String.format("%02x", b));
-        }
-        return builder.toString();
-    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Input flag: ");
@@ -18,25 +11,25 @@ public class Flagger {
             System.out.println("Incorrect flag length");
             System.exit(1);
         } else if (!flag.endsWith("p}")) {
-            System.out.println("Incorrect flag");
+            System.out.println("Incorrect flag 1");
             System.exit(1);
         } else if (!flag.startsWith("SgffCTF{")) {
-            System.out.println("Incorrect flag");
+            System.out.println("Incorrect flag 2");
             System.exit(1);
         } else if (!flag.substring(27, 30).equals("u_u")) {
-            System.out.println("Incorrect flag");
+            System.out.println("Incorrect flag 3");
             System.exit(1);
         } else if (!flag.substring(12, 17).equals("6_55r")) {
-            System.out.println("Incorrect flag");
+            System.out.println("Incorrect flag 4");
             System.exit(1);
         } else if (!flag.substring(17, 23).equals("12gf11")) {
-            System.out.println("Incorrect flag");
+            System.out.println("Incorrect flag 5");
             System.exit(1);
         } else if (!flag.substring(23, 27).equals("5_pp")) {
-            System.out.println("Incorrect flag");
+            System.out.println("Incorrect flag 6");
             System.exit(1);
         } else if (!flag.substring(8, 12).equals("pvpv")) {
-            System.out.println("Incorrect flag");
+            System.out.println("Incorrect flag 7");
             System.exit(1);
         }
         System.out.println("Yeah! Your flag is " + flag);
